d5885963418964540c12b5e06ab2f7ae35365b87
c00p! <a1@c00p-3.local>
2017-05-19 13:36:59 +0300

    fixed laguage bug

--- js.js
+++ js.js
@@ -6,23 +6,32 @@ var enLang = {  "text" : "hithere",
                 "adv" : "Here could be your advertisement"};
 
 
-langChange(); //default language
-var text = document.getElementById('text');
 var btnUa = document.getElementById('btn-ua');
 var btnEn = document.getElementById('btn-eng');
 var userInp = document.getElementById('user-input');
+var text = document.getElementById('text1');
+
+var mistake = document.getElementById('mistake');
+var adv = document.getElementById('adv');
+var data = uaLang; //default ua
+langChange(); //default language
 
 btnUa.addEventListener('click', langChange);
 btnEn.addEventListener('click', langChange);
 
 
 function langChange (e) {
-    var data = uaLang; //default ua
-    if (e.target.id === "btn-eng") {
-        // console.log('eng');
-        data = enLang;
-    } else {
-        // console.log('ua');
-        data = uaLang;
+    if (e !== undefined) {
+        if (e.target.id === "btn-eng") {
+            console.log('eng');
+            data = enLang;
+        } else {
+            console.log('ua');
+            data = uaLang;
+        }
     }
+    // console.log(data['text']);
+    text.innerHTML = data['text'];
+    mistake.innerHTML = data['mistake'];
+    adv.innerHTML = data['adv'];
 }
