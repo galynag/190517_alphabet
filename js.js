d5e74ec14f07c5f829bc8a1430f36b2055b26e34
c00p! <a1@c00p-3.local>
2017-05-19 12:15:11 +0300

    initial js

--- js.js
+++ js.js
@@ -1,3 +1,28 @@
-/**
- * Created by UpscaleAcademy on 19.05.2017.
- */
+var uaLang = {  "text" : "zdarova",
+                "mistake" : "oshibochka vishla",
+                "adv" : "zdes' mogla by byt' vasha reklama"};
+var enLang = {  "text" : "hithere",
+                "mistake" : "it's a mistake",
+                "adv" : "Here could be your advertisement"};
+
+
+langChange(); //default language
+var text = document.getElementById('text');
+var btnUa = document.getElementById('btn-ua');
+var btnEn = document.getElementById('btn-eng');
+var userInp = document.getElementById('user-input');
+
+btnUa.addEventListener('click', langChange);
+btnEn.addEventListener('click', langChange);
+
+
+function langChange (e) {
+    var data = uaLang; //default ua
+    if (e.target.id === "btn-eng") {
+        // console.log('eng');
+        data = enLang;
+    } else {
+        // console.log('ua');
+        data = uaLang;
+    }
+}
